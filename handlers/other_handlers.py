ea1cb154297fb4dc8516b0b092d49af1d3a34a80
Seleznyov Alexandr <Selezeni@yahoo.com>
2022-12-15 15:57:33 +0300

    Правки

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -2,9 +2,11 @@ from aiogram import Dispatcher
 from aiogram.types import Message
 
 
+# Этот хэндлер будет реагировать на любые текстовые сообщения пользователя
 async def send_echo(message: Message):
-    await message.answer(f"Это эхо! {message.text}")
+    await message.answer(f'Это эхо! {message.text}')
 
 
+# Функция для регистрации хэндлера
 def register_echo_handler(dp: Dispatcher):
     dp.register_message_handler(send_echo)
