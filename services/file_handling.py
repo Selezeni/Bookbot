f7e5889d82974528e1b24fb7b2bd39767b3a2036
Seleznyov Alexandr <Selezeni@yahoo.com>
2022-12-15 12:02:42 +0300

    Правки

--- services/file_handling.py
+++ services/file_handling.py
@@ -8,8 +8,8 @@ book: dict[int, str] = {}
 def _get_part_text (text: str, start: int, page_size: int) -> tuple[str, int]:
     sign = ",.!:;?"
     page = text[start:start + page_size]
-    if page[-2] in sign:
-        page = page[:-2]
+    # if page[-2] in sign:
+    #     page = page[:-2]
     while page[-1] not in sign:
         page = page[:-1]
     return page, len(page)
@@ -17,7 +17,15 @@ def _get_part_text (text: str, start: int, page_size: int) -> tuple[str, int]:
 
 # Функция, формирующая словарь книги
 def prepare_book(path: str) -> None:
-    pass
+    with open(path, encoding='utf-8') as file:
+        text = file.read()
+    global PAGE_SIZE, book
+    start = counter = 0
+    while start < len(text):
+        counter += 1
+        prepared_page = _get_part_text(text, start, PAGE_SIZE)
+        book[counter] = prepared_page[0].lstrip()
+        start += prepared_page[1]
 
 
 # Вызов функции prepare_book для подготовки книги из текстового файла
